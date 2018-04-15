aad07202d3d67585ace1ce56ecc50c06978bc5f3
hanzeljesheen <hanzel@botleg.com>
2018-04-15 17:36:43 +0530

    implement commit webhook route

---
+++ handlers/commit.js
@@ -0,0 +1,44 @@
+'use strict'
+const axios = require('axios')
+const ESClient = require('../utils/es-client')
+const SlackClient = require('../utils/slack-client')
+
+exports.handler = async event => {
+  try {
+    const body = JSON.parse(event.body)
+    const es = new ESClient()
+    const slackClient = new SlackClient()
+
+    for (var commit of body.commits) {
+      const added = commit.added.filter(item => item.includes('_posts/'))
+      for (let story of added) {
+        const res = await axios(process.env.GITHUB_BASE + story)
+        const id = story.substring(25, story.length - 9)
+        await es.indexDoc(id, res.data)
+        await slackClient.updateStory('Created', res.data.split('\n')[2].slice(7))
+      }
+
+      const removed = commit.removed.filter(item => item.includes('_posts/'))
+      for (let story of removed) {
+        const id = story.substring(25, story.length - 9)
+        await es.deleteDoc(id)
+        await slackClient.updateStory('Deleted', id.split('-').join(' '))
+      }
+
+      const modified = commit.modified.filter(item => item.includes('_posts/'))
+      for (let story of modified) {
+        const res = await axios(process.env.GITHUB_BASE + story)
+        const id = story.substring(25, story.length - 9)
+        await es.updateDoc(id, res.data)
+        await slackClient.updateStory('Modified', res.data.split('\n')[2].slice(7))
+      }
+    }
+
+    return (null, {
+      statusCode: 204,
+      body: null
+    })
+  } catch (err) {
+    console.error(err.stack)
+  }
+}
